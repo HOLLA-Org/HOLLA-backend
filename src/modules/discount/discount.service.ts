185f753189cfdbd5285fa401697ad4805e7904aa
minhvh5504 <vihongminh5504@gmail.com>
2025-08-20 23:31:14 +0700

    feat: create api help apply discount payment

--- src/modules/discount/discount.service.ts
+++ src/modules/discount/discount.service.ts
@@ -1,14 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateDiscountDto } from './dto/create-discount.dto';
 import { UpdateDiscountDto } from './dto/update-discount.dto';
 import { Discount, DiscountDocument } from './schemas/discount.schema';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from '../users/schemas/user.schema';
 
 @Injectable()
 export class DiscountService {
   constructor(
     @InjectModel(Discount.name) private discountModel: Model<DiscountDocument>,
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
   ) {}
 
   async create(createDiscountDto: CreateDiscountDto): Promise<Discount> {
@@ -21,9 +23,49 @@ export class DiscountService {
     const newDiscount = new this.discountModel(createDiscountDto);
     return newDiscount.save();
   }
+  async applyDiscount(
+    code: string,
+    user_id: string,
+  ): Promise<{ value: number }> {
+    const existingUser = await this.userModel.findById(user_id);
+    if (!existingUser) {
+      throw new BadRequestException(`User with id ${user_id} not found`);
+    }
 
-  findAll() {
-    return `This action returns all discount`;
+    const discount = await this.discountModel.findOne({ code });
+    if (!discount) {
+      throw new BadRequestException('Discount not found');
+    }
+
+    if (discount.expires_at && discount.expires_at < new Date()) {
+      throw new BadRequestException('Discount expired');
+    }
+
+    const used = discount.used_by.find((u) => u.user_id.toString() === user_id);
+
+    if (used) {
+      if (used.used_count >= discount.max_usage) {
+        throw new BadRequestException(
+          'Discount usage limit reached for this user',
+        );
+      }
+
+      await this.discountModel.updateOne(
+        { _id: discount._id, 'used_by.user_id': user_id },
+        { $inc: { 'used_by.$.used_count': 1 } },
+      );
+    } else {
+      await this.discountModel.updateOne(
+        { _id: discount._id },
+        {
+          $push: {
+            used_by: { user_id, used_count: 1 },
+          },
+        },
+      );
+    }
+
+    return { value: discount.value };
   }
 
   findOne(id: number) {
